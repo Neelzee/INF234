dbb902bbb91e436e1a2bb5372984201834541819
Nils Michael <90567729+Neelzee@users.noreply.github.com>
2023-10-02 08:44:10 +0200

    Updated explanation text

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 """
-Using Kahn's algorithm, I can sort any Vertex.
+Using Kahn's algorithm, I can sort any DAG.
 The reason for this, is that, when constructing the Vertex,
 you have to supply the vertices that are pointing towards the one you are constructing.
 So, given the graph (list), I can sort it, based on the length of _from.
 
-This makes the running time of the algorithm to be N, since we have to take the length of _from.
+This makes the running time of the algorithm to be `N log N`, since we have to take the length of _from, when sorting, like so: vertices.sort(key = lambda v: len(v._from))-
+The big-o of `len()` is `1`.
+While the `.sort()` method, has a big-o of `N log N`, making the running time of the entire algorithm, `N log N`. Where `N` is the amount of vertices in the DAG.
 
-This could be called 'cheating', so instead, we can do the following:
+This could be called 'cheating', or a useless algorithm, since we need the in-degrees of a Vertex.
+If this was to be a realistic, and or usefull algorithm, a Vertex should only keep track of the vertices it is pointing towards, so instead, we can do the following:
+
+1. Change Vertex from needing the vertices that are pointing towards it, to vertices that it is pointing too.
+
+2. Create a function, f(vertices: Graph[Vertex]) -> dict[Vertex, Int]
+    2. 1. This function takes in all the vertices in a Graph, and should return a dictionary, of each Vertex, and their corresponding in-degree.
+    2. 2. Too achive this, we iterate over all the vertices _v_, and iterate over _v_ 's neighbours (the vertices _v_ is pointing towards). Incrementing their count in the dictionary.
+    2. 3. We then use this function, like so: verticies.sort(key = lamba v: dictionary[v]), where dictionary is created by `f`.
+
+3. This gives us a big-o of `N^2`.
 
 Author: Nils Michael Fitjar
 """
